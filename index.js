1e6fcfa35ab86d242bc000d6d1201fcf371288b5
Marcin Rojek <marcin.rojek@outlook.com>
2020-03-21 17:01:26 +0100

    new

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const app = express();
 const port = 3001;
 
 app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/about", (req, res) => res.send("about"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
